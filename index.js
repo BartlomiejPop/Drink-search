fb391ef5fc262ee2b57ad6e928ed28e02398f01e
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-03-28 10:36:49 +0200

    a

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const inputFilterEl = document.getElementById("filter");
 const drinksEl = document.querySelectorAll("li");
+const listEl = document.querySelector("#list");
 
 const search = (e) => {
-    
-    const text = e.target.value.toLowerCase();
-    
-    drinksEl.forEach((item) => {
-        
+	const text = e.target.value.toLowerCase();
+
+	drinksEl.forEach((item) => {
 		if (item.textContent.toLowerCase().includes(text)) {
 			return (item.style.display = "block");
 		}
 		return (item.style.display = "none");
 	});
+};
+
+const showDeleteBtn = (id) => {
+	const deleteBtn = listEl.children[id].childNodes[1];
+	deleteBtn.style.display = "block";
+	deleteBtn.addEventListener("click", () => {
+		console.log(listEl.children[id]);
+		listEl.removeChild(listEl.children[id]);
+	});
+};
+
+const hideDeleteBtn = (id) => {
+	const deleteBtn = listEl.children[id].childNodes[1];
+	deleteBtn.style.display = "none";
 };
 
 inputFilterEl.addEventListener("input", search);
