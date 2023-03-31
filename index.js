6fece599424d6d3b53ccb405479e617b0ba9d0a4
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-03-31 12:13:47 +0200

    a

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const addDrinkBtn = document.querySelector(".add-drink");
 const modalEl = document.querySelector(".add-drink-modal");
 const modalShadowEl = document.querySelector(".modal-shadow");
 const submitBtn = document.querySelector(".submit");
-const addNameEl = document.querySelector(".add-name");
-const addDescriptionEl = document.querySelector(".add-description");
+const addNameEl = document.querySelector(".add-drink-input");
+const addDescriptionEl = document.querySelector(".add-textarea");
+const spanPopupEl = document.querySelector(".span-popup");
+const descriptionModalEl = document.querySelector(".description-modal");
+const descriptionEl = document.querySelector(".description");
+let idCounter = 27;
 
 const search = (e) => {
 	const text = e.target.value.toLowerCase();
@@ -40,17 +44,52 @@ const hideDeleteBtn = (id) => {
 const showModal = () => {
 	modalEl.style.display = "flex";
 	modalShadowEl.style.display = "block";
+	spanPopupEl.style.display = "none";
 	modalShadowEl.addEventListener("click", hideModal);
-	submitBtn.addEventListener("click", addElement);
+	submitBtn.addEventListener("click", () => {
+		addElement();
+	});
 };
 
-const addElement = () => {};
+const addElement = () => {
+	if (addDescriptionEl.value === "" || addNameEl.value === "") {
+		spanPopupEl.style.display = "block";
+		return;
+	}
+	listEl.innerHTML += `
+	<li
+							id=${idCounter}
+							onmouseenter="showDeleteBtn(${idCounter})"
+							onmouseleave="hideDeleteBtn(${idCounter})"
+							onclick="showDescription(${idCounter})">
+							${addNameEl.value}<i
+								style="display: none"
+								class="fa-solid fa-minus"></i>
+						</li>`;
+	spanPopupEl.style.display = "none";
+	addDescriptionEl.value = "";
+	addNameEl.value = "";
+	idCounter++;
+	hideModal();
+};
 
 const hideModal = () => {
 	modalEl.style.display = "none";
 	modalShadowEl.style.display = "none";
+	descriptionModalEl.style.display = "none";
+	descriptionEl.style.display = "none";
+};
+
+const showDescription = (id) => {
+	const productDescription = document.getElementById(id).attributes[4].value;
+	modalShadowEl.style.display = "block";
+	descriptionModalEl.style.display = "block";
+	descriptionEl.style.display = "block";
+	descriptionEl.textContent = productDescription;
+	modalShadowEl.addEventListener("click", hideModal);
 };
 
 inputFilterEl.addEventListener("input", search);
 
 addDrinkBtn.addEventListener("click", showModal);
+console.log(descriptionEl.textContent);
