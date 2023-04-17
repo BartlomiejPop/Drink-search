9f3d0c631a4d19886e20aaf269d536c131cf7728
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-04-17 02:01:01 +0200

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+import Notiflix from "notiflix";
 const inputFilterEl = document.getElementById("filter");
-const drinksEl = document.querySelectorAll("li");
 const listEl = document.querySelector("#list");
 const addDrinkBtn = document.querySelector(".add-drink");
 const modalEl = document.querySelector(".add-drink-modal");
@@ -16,21 +16,13 @@ const deletedDrinksListEl = document.querySelector(".deleted-drinks-list");
 const deleteCancelBtn = document.querySelector(".delete-popup-cancel");
 const deletePermamentBtn = document.querySelector(".delete-popup-remove");
 const deletePopupEl = document.querySelector(".delete-popup-div");
+const clearStorageBtn = document.querySelector(".clear-storage");
+const deletePopupTextEl = document.querySelector(".delete-popup-text");
 let idCounter = 27;
 let localCounter = 0;
 let deletedDrinksArr = [];
 let addEl;
-
-const search = (e) => {
-	const text = e.target.value.toLowerCase();
-
-	drinksEl.forEach((item) => {
-		if (item.textContent.toLowerCase().includes(text)) {
-			return (item.style.display = "flex");
-		}
-		return (item.style.display = "none");
-	});
-};
+let storagedDrinks = [];
 
 const showDeleteBtn = (id) => {
 	const deleteBtn = document.getElementById(id).childNodes[1];
@@ -39,9 +31,30 @@ const showDeleteBtn = (id) => {
 		"click",
 		_.throttle(() => {
 			if (document.getElementById(id)) {
-				deletedDrinksArr.push(document.getElementById(id));
+				deletedDrinksArr.push({
+					id: document.getElementById(id).id,
+					name: document.getElementById(id).innerText,
+					description: document.getElementById(id).attributes[1].value,
+				});
+				console.log(deletedDrinksArr);
+
+				localStorage.setItem(
+					`drinksToDelete`,
+					JSON.stringify(deletedDrinksArr)
+				);
+
+				// deletedDrinksArr = deletedDrinksArr.filter((el) => el.id != id);
+
+				storagedDrinks = storagedDrinks.filter((el) => +el.id !== id);
+
+				localStorage.setItem(`savedDrinks`, JSON.stringify(storagedDrinks));
 			}
-			console.log(deletedDrinksArr);
+			if (document.getElementById(id).id < 26) {
+				Notiflix.Notify.info(
+					"Elementy początkowe nie mogą być usunięte permamentnie z listy. Usuwanie ich może doprowadzić do duplikacji"
+				);
+			}
+
 			listEl.removeChild(document.getElementById(id));
 			if (deletedDrinksArr.length > 0) {
 				deletedDrinksBtn.style.display = "block";
@@ -74,26 +87,32 @@ const addElement = () => {
 		spanPopupEl.style.display = "block";
 		return;
 	}
-	listEl.insertAdjacentHTML(
+	const newDrink = listEl.insertAdjacentHTML(
 		"beforeend",
 		`
 	<li
 							id=${idCounter}
-							onmouseenter="showDeleteBtn(${idCounter})"
-							onmouseleave="hideDeleteBtn(${idCounter})"
-							onclick="showDescription(${idCounter})"
 							description="${addDescriptionEl.value}">
 							${addNameEl.value}<i
 								style="display: none"
 								class="fa-regular fa-square-minus"></i>
 						</li>`
 	);
+	storagedDrinks.push({
+		id: idCounter,
+		description: addDescriptionEl.value,
+		name: addNameEl.value,
+	});
+
+	localStorage.setItem(`savedDrinks`, JSON.stringify(storagedDrinks));
 
 	addDescriptionEl.value = "";
 	addNameEl.value = "";
 	spanPopupEl.style.display = "none";
 	idCounter++;
 	hideModal();
+	handleDeleteBtn();
+	Notiflix.Notify.success("Pomyślnie dodane napój");
 };
 
 const hideModal = () => {
@@ -111,7 +130,7 @@ const hideModal = () => {
 };
 
 const showDescription = (id) => {
-	const productDescription = document.getElementById(id).attributes[4].value;
+	const productDescription = document.getElementById(id).attributes[1].value;
 	modalShadowEl.style.opacity = "1";
 	descriptionModalEl.style.opacity = "1";
 	modalShadowEl.style.display = "block";
@@ -139,24 +158,32 @@ const showDeletedDrinks = () => {
 	deletedDrinksArr.forEach((el) => {
 		if (el) {
 			addEl = document.createElement("li");
-			addEl.innerHTML = `<li id="${el.id}" class="deleted-drink class${el.id}" description="${el.attributes[4].textContent}">${el.textContent}<div><button onclick="saveElement(${el.id})" class="save"><i class="fa-solid fa-share"></i></button><button onclick="deleteElement(${el.id})" class="delete-permament"><i class="fa-solid fa-xmark"></i></button></div></li>`;
+			addEl.innerHTML = `<li id="${el.id}" class="deleted-drink class${el.id}" description="${el.description}">${el.name}<div><button  class="save"><i class="fa-solid fa-share"></i></button><button class="delete-permament"><i class="fa-solid fa-xmark"></i></button></div></li>`;
 			deletedDrinksListEl.appendChild(addEl);
-
-			// deletedDrinksListEl.innerHTML += ` <li id="${el.id}" class="deleted-drink" description="${el.attributes[4].textContent}">${el.textContent}<div><button onclick="saveElement(${el.id})" class="save"><i class="fa-solid fa-share"></i></button><button onclick="deleteElement(${el.id})" class="delete-permament"><i class="fa-solid fa-xmark"></i></button></div></li>`;
 		}
 	});
+	const saveBtns = document.querySelectorAll(".save");
+	const deleteBtns = document.querySelectorAll(".delete-permament");
+	saveBtns.forEach((el) =>
+		el.addEventListener("click", (e) => {
+			saveElement(+e.currentTarget.parentElement.parentElement.id);
+		})
+	);
+	deleteBtns.forEach((el) =>
+		el.addEventListener("click", (e) => {
+			deleteElement(+e.currentTarget.parentElement.parentElement.id);
+		})
+	);
 };
 
 const saveElement = (id) => {
+	console.log(document.getElementById(id));
 	const savingEl = listEl.insertAdjacentHTML(
 		"beforeend",
 		`
 	<li
 							id=${id}
-							onmouseenter="showDeleteBtn(${id})"
-							onmouseleave="hideDeleteBtn(${id})"
-							onclick="showDescription(${id})"
-							description="${document.getElementById(id).attributes[2].textContent}">
+							description="${document.getElementById(id).attributes[1].textContent}">
 							${document.getElementById(id).textContent}<i
 								style="display: none"
 								class="fa-regular fa-square-minus"></i>
@@ -169,8 +196,12 @@ const saveElement = (id) => {
 	deletedDrinksArr.forEach((el) => {
 		if (el.id == id) {
 			deletedDrinksArr.splice(deletedDrinksArr.indexOf(el), 1);
+			storagedDrinks.push(el);
+			localStorage.setItem("savedDrinks", JSON.stringify(storagedDrinks));
 		}
 	});
+
+	localStorage.setItem("drinksToDelete", JSON.stringify(deletedDrinksArr));
 
 	if (deletedDrinksArr.length === 0) {
 		deletedDrinksBtn.style.display = "none";
@@ -186,11 +217,13 @@ const saveElement = (id) => {
 		const deleteBtn = document.getElementById(id).childNodes[1];
 		deleteBtn.style.display = "block";
 	}
+	handleDeleteBtn();
 };
+
 const deleteElement = (id) => {
+	deletePopupTextEl.textContent = "Usunąć permamentnie?";
 	deletePopupEl.style.display = "flex";
 	document.addEventListener("click", (e) => {
-		console.log(e.target);
 		if (e.target === deleteCancelBtn) {
 			deletePopupEl.style.display = "none";
 		} else if (e.target === deletePermamentBtn) {
@@ -200,6 +233,11 @@ const deleteElement = (id) => {
 			deletedDrinksArr.forEach((el) => {
 				if (el.id == id) {
 					deletedDrinksArr.splice(deletedDrinksArr.indexOf(el), 1);
+					deletedDrinksArr = deletedDrinksArr.filter((el) => el.id != id);
+					localStorage.setItem(
+						"drinksToDelete",
+						JSON.stringify(deletedDrinksArr)
+					);
 					if (deletedDrinksArr.length === 0) {
 						deletedDrinksBtn.style.display = "none";
 						deletedDrinksModalEl.style.opacity = "0";
@@ -213,6 +251,9 @@ const deleteElement = (id) => {
 				}
 			});
 			deletePopupEl.style.display = "none";
+			if (+drinkToDelete.id > 25) {
+				Notiflix.Notify.success("Usunięto permamentnie.");
+			}
 		} else if (e.target === modalShadowEl) {
 			deletePopupEl.style.display = "none";
 		}
@@ -224,6 +265,85 @@ const deleteElement = (id) => {
 	}
 };
 
+const search = (e) => {
+	const text = e.target.value.toLowerCase();
+	const drinksEl = document.querySelectorAll("li");
+
+	drinksEl.forEach((item) => {
+		if (item.textContent.toLowerCase().includes(text)) {
+			return (item.style.display = "flex");
+		}
+		return (item.style.display = "none");
+	});
+};
+
+const updateList = () => {
+	if (localStorage.getItem("savedDrinks")) {
+		JSON.parse(localStorage.getItem("savedDrinks")).forEach((el) =>
+			storagedDrinks.push(el)
+		);
+
+		JSON.parse(localStorage.getItem("savedDrinks")).forEach((el) =>
+			listEl.insertAdjacentHTML(
+				"beforeend",
+				`<li
+							id=${el.id}
+							description="${el.description}">
+							${el.name}<i
+								style="display: none"
+								class="fa-regular fa-square-minus"></i>
+						</li>`
+			)
+		);
+	}
+	if (localStorage.getItem("drinksToDelete")) {
+		JSON.parse(localStorage.getItem("drinksToDelete")).forEach((el) =>
+			deletedDrinksArr.push(el)
+		);
+	}
+	if (deletedDrinksArr.length) {
+		deletedDrinksBtn.style.display = "block";
+	}
+	idCounter += storagedDrinks.length;
+};
+
 inputFilterEl.addEventListener("input", search);
 addDrinkBtn.addEventListener("click", showModal);
 deletedDrinksBtn.addEventListener("click", showDeletedDrinks);
+clearStorageBtn.addEventListener("click", () => {
+	deletePopupTextEl.textContent =
+		"Usunąć pamięć lokalną i zresetować aplikację?";
+	deletePopupEl.style.display = "flex";
+	document.addEventListener("click", (e) => {
+		if (e.target === deletePermamentBtn) {
+			localStorage.clear();
+			deletePopupEl.style.display = "none";
+			Notiflix.Notify.success(
+				"Pamięć lokalna usunięta. Strona zostanie przywrócona do stanu pierwotnego po odświerzeniu."
+			);
+		} else if (e.target === deleteCancelBtn) {
+			deletePopupEl.style.display = "none";
+		}
+	});
+});
+
+const handleDeleteBtn = () => {
+	const listItemEl = document.querySelectorAll("li");
+
+	listItemEl.forEach((el) => {
+		el.addEventListener("mouseover", (e) => {
+			showDeleteBtn(+e.target.id);
+		}),
+			el.addEventListener("mouseleave", (e) => {
+				hideDeleteBtn(+e.target.id);
+			}),
+			el.addEventListener("click", (e) => {
+				if (e.target.nodeName !== "I") {
+					showDescription(+e.target.id);
+				}
+			});
+	});
+};
+
+updateList();
+handleDeleteBtn();
